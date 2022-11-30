e86403e2efcb2222ce98a40fd7426cc9517b922f
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-11-30 00:22:22 -0500

    refactor: reorganize ts-belt code

---
+++ src/ts-belt/result/result.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+import { pipe, R } from '@mobily/ts-belt';
+
+interface Foo {
+  status: 200 | 400;
+  body: string;
+}
+
+const success = (): Promise<Foo> =>
+  Promise.resolve({
+    status: 200,
+    body: 'success',
+  });
+
+const failure = (body: string): Foo => ({
+  status: 400,
+  body,
+});
+
+console.log(
+  await pipe(
+    R.Ok('foo'),
+    R.match(success, () => Promise.resolve(failure('failure')))
+  )
+);
+
+console.log(
+  await pipe(
+    R.Error('failure'),
+    R.match(success, (message) => Promise.resolve(failure(message)))
+  )
+);
