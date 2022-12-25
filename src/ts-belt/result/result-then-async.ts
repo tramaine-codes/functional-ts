8f282de8cb08222ccd9dcae49347199915dbd604
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-12-25 00:54:36 -0500

    refactor: remove if-else statement

--- src/ts-belt/result/result-then-async.ts
+++ src/ts-belt/result/result-then-async.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
-import { pipe, R } from '@mobily/ts-belt';
+import { AR, pipe, R } from '@mobily/ts-belt';
 
-function validate(num: number) {
+async function validate(num: number) {
   if (num < 10) {
     return R.Error(['foo', 'bar'] as const);
   }
@@ -11,15 +11,20 @@ function validate(num: number) {
   } as const);
 }
 
+// function validate2(num: number) {
+//   if (num < 10) {
+//     return AR.reject(['foo', 'bar'] as const);
+//   }
+
+//   return AR.resolve({
+//     baz: 'qux',
+//   } as const);
+// }
+
 function process({ baz }: { readonly baz: string }) {
-  return Promise.resolve(baz);
+  return AR.resolve(baz);
 }
 
-const result = pipe(1, validate, R.map(process));
-// const result = pipe(11, validate, R.map(process));
+const result = await pipe(11, validate, AR.flatMap(process));
 
-if (R.isOk(result)) {
-  console.log(R.Ok(await pipe(result, R.getExn)));
-} else {
-  console.log(result);
-}
+console.log(result);
