ee00463ca7967f3fe6d19d3998cb6b45dde0ae79
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-12-26 20:12:35 -0500

    feat: add immutable list and range examples

---
+++ src/immutable/list.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+import { List, Range } from 'immutable';
+// import L from 'list';
+
+const lst = List([1, 2, 3, 4, 5]);
+
+console.log(lst);
+console.log(lst.reduce((acc, x) => acc + x, 0, lst));
+console.log(lst.toArray());
+console.log(Range(6, 10).toArray());
