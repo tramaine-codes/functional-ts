936101a6df24b257ec08ae4cf3191c2d9738668d
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-12-26 21:39:38 -0500

    refactor: reorder users in order to sort them

--- src/remeda/array.ts
+++ src/remeda/array.ts
@@ -2,11 +2,11 @@
 import * as R from 'remeda';
 
 const users = [
-  { name: 'john', age: 20, gender: 'm' },
-  { name: 'marry', age: 22, gender: 'f' },
   { name: 'samara', age: 24, gender: 'f' },
-  { name: 'paula', age: 24, gender: 'f' },
+  { name: 'john', age: 20, gender: 'm' },
   { name: 'bill', age: 33, gender: 'm' },
+  { name: 'marry', age: 22, gender: 'f' },
+  { name: 'paula', age: 24, gender: 'f' },
 ];
 
 console.log(
