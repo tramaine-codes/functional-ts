ecdfbd504b02303eec9c052dfeadb35e01e51e75
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-12-28 03:26:12 -0500

    refactor: remove unnecessary function

--- src/purify-ts/option/ap.ts
+++ src/purify-ts/option/ap.ts
@@ -1,10 +1,6 @@
 /* eslint-disable no-console */
 import { Just, List } from 'purify-ts';
 
-function get<T>(index: number, lst: T[]) {
-  return List.at(index, lst);
-}
-
 const add = (x: number) => (y: number) => x + y;
 
 console.log(
@@ -14,13 +10,13 @@ console.log(
 );
 
 console.log(
-  get(1, [1, 2, 3])
+  List.at(1, [1, 2, 3])
     .ap(Just(3).ap(Just(add)))
     .extract()
 );
 
 console.log(
-  get(3, [1, 2, 3])
+  List.at(3, [1, 2, 3])
     .ap(Just(3).ap(Just(add)))
     .extract()
 );
