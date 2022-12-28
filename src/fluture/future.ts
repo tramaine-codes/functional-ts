970e399c2d38c10eaad221fe99649c872c853b01
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-12-27 22:19:12 -0500

    feat: add fluture

---
+++ src/fluture/future.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+import Future, {
+  attempt,
+  attemptP,
+  chainRej,
+  map,
+  mapRej,
+  promise,
+  reject,
+  resolve,
+  value,
+} from 'fluture';
+import { noop } from 'remeda';
+
+const future = reject(20)
+  .pipe(mapRej((x) => x + 1))
+  .pipe(chainRej((x) => resolve(x + x)));
+
+future.pipe(value(console.log));
+
+const foo = await promise(
+  Future<Error, string>((_reject, resolve) => {
+    resolve('foo');
+    return noop;
+  }).pipe(map((val) => `${val}!`))
+);
+
+console.log(foo);
+
+const bar = await promise(
+  attempt<Error, string>(() => 'bar').pipe(map((val) => `${val}!`))
+);
+
+console.log(bar);
+
+const baz = await promise(
+  attemptP<Error, string>(async () => 'baz').pipe(map((val) => `${val}!`))
+);
+
+console.log(baz);
