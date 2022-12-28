b12954b8d84a88f193fdaa25e68959b9e9bff14c
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-12-27 23:32:32 -0500

    chore: remove commented out code

--- src/santuary/array.ts
+++ src/santuary/array.ts
@@ -9,19 +9,11 @@ const users = [
   { name: 'paula', age: 24, gender: 'f' },
 ];
 
-// console.log(S.filter((x: { gender: string }) => x.gender === 'f')(users));
+console.log(S.filter((x: { gender: string }) => x.gender === 'f')(users));
+
 console.log(
   S.pipe([
     S.filter((x: { gender: string; age: number }) => x.gender === 'f'),
     S.sortBy(S.prop('age')),
   ])(users)
 );
-
-// console.log(
-//   S.pipe(
-//     // users,
-//     S.filter((x: { gender: string; }) => x.gender === 'f')
-//     // S.filter((x) => x.gender === 'f')
-//     // R.groupBy((x) => x.age)
-//   )(users)
-// );
