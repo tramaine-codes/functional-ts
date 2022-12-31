7ce2f2092f96a105007f45732273312daa64a6e8
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-12-31 01:10:23 -0500

    feat: add shades

---
+++ src/shades/lens.ts
@@ -0,0 +1,71 @@
+/* eslint-disable no-console */
+import { pipe } from 'purifree-ts';
+import S from 'shades';
+
+const jack = {
+  name: 'Jack Sparrow',
+  goldMember: false,
+  posts: [
+    {
+      title:
+        'Why is the rum always gone? An analysis of Carribean trade surplus',
+      likes: 5,
+    },
+    {
+      title: 'Sea Turtles - The Tortoise and the Hair',
+      likes: 70,
+    },
+  ],
+};
+
+const liz = {
+  name: 'Elizabeth Swan',
+  goldMember: true,
+  posts: [
+    {
+      title: 'Bloody Pirates - My Life Aboard the Black Pearl',
+      likes: 10000,
+    },
+    {
+      title:
+        'Guidelines - When YOU need to be disinclined to acquiesce to their request',
+      likes: 5000,
+    },
+  ],
+};
+
+const bill = {
+  name: 'Bill Turner',
+  goldMember: false,
+  posts: [
+    {
+      title: 'Bootstraps Bootstraps - UEFI, GRUB and the Linux Kernel',
+      likes: 3000,
+    },
+  ],
+};
+
+const store = {
+  users: [jack, liz, bill],
+  byName: {
+    jack,
+    liz,
+    bill,
+  },
+};
+
+console.log(pipe(jack, S.get('name')));
+
+console.log(pipe(store, S.get('users', 0, 'name')));
+
+console.log(pipe(store, S.get('users', S.all(), 'posts')));
+
+console.log(pipe(store, S.get('users', S.all(), 'posts', S.all(), 'likes')));
+
+console.log(
+  pipe(
+    store,
+    S.mod('users', 0, 'posts', 0, 'likes')((likes: number) => likes + 1),
+    S.get('users', 0, 'posts', 0, 'likes')
+  )
+);
