4e708e35a03709d49571df53dce205120636806c
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2023-10-27 19:22:20 -0400

    chore: upgrade dependencie

--- src/effect/effect.ts
+++ src/effect/effect.ts
@@ -1,5 +1,25 @@
-import { Console, Effect } from 'effect';
+import { Console, Effect, pipe } from 'effect';
 
 const program = Console.log('Hello, World!');
 
 Effect.runSync(program);
+
+const divide = (a: number, b: number): Effect.Effect<never, Error, number> =>
+  b === 0
+    ? Effect.fail(new Error('cannot divide by zero'))
+    : Effect.succeed(a / b);
+
+pipe(
+  divide(1, 2),
+  // Effect.match({
+  //   onFailure: (error) => error.message,
+  //   onSuccess: (value) => value,
+  // }),
+  Effect.mapBoth({
+    onFailure: (error) => error.message,
+    onSuccess: (value) => value,
+  }),
+  (x) => x,
+  Console.log,
+  Effect.runSync
+);
