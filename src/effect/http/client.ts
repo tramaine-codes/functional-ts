a1acc0dda077a506830a900f518704ceead0ef27
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2023-12-24 19:56:01 -0500

    feat: add http client and schema examples

---
+++ src/effect/http/client.ts
@@ -0,0 +1,26 @@
+import * as Http from '@effect/platform/HttpClient';
+import * as Schema from '@effect/schema/Schema';
+import { Console, Effect } from 'effect';
+import { constVoid, pipe } from 'effect/Function';
+
+const Post = Schema.struct({
+  id: Schema.number,
+  body: Schema.string,
+  title: Schema.string,
+  userId: Schema.number,
+});
+
+await pipe(
+  Http.request.get('https://jsonplaceholder.typicode.com/posts/1'),
+  Http.client.fetch(),
+  Effect.flatMap(Http.response.schemaBodyJson(Post)),
+  Effect.tapBoth({
+    onFailure: Console.log,
+    onSuccess: Console.log,
+  }),
+  Effect.match({
+    onFailure: constVoid,
+    onSuccess: constVoid,
+  }),
+  Effect.runPromise
+);
