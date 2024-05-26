16f289e177517a0c222e949acddc9c795639cb57
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-05-26 19:04:47 -0400

    feat: add try to either

--- src/primitives/either.ts
+++ src/primitives/either.ts
@@ -10,21 +10,32 @@ abstract class Either<L, R> {
 
   abstract chain: <R2>(f: (x: R) => R2) => R2;
 
-  abstract map: <R2>(f: (x: R) => R2) => Either<L, R2>;
-
   abstract fold: <L2, R2>(o: {
     onLeft: (x: L) => L2;
     onRight: (x: R) => R2;
   }) => L2 | R2;
 
-  [util.inspect.custom] = () => `${this._tag}(${util.inspect(this.x)})`;
+  abstract map: <R2>(f: (x: R) => R2) => Either<L, R2>;
 
-  static right = <R>(x: R) => Right.of(x);
+  abstract mapLeft: <L2>(f: (x: L) => L2) => Either<L2, R>;
+
+  protected [util.inspect.custom] = () =>
+    `${this._tag}(${util.inspect(this.x)})`;
+
+  static fromNullable = <T>(x: T) =>
+    x !== null && x !== undefined ? Right.of(x) : Left.of('Left');
 
   static left = <L>(x: L) => Left.of(x);
 
-  static fromNullable = <T>(x: T) =>
-    x !== null && x !== undefined ? Right.of(x) : Left.of(1);
+  static right = <R>(x: R) => Right.of(x);
+
+  static try = <R2, L2 = unknown>(f: () => R2): Either<L2, R2> => {
+    try {
+      return Right.of(f());
+    } catch (error) {
+      return Left.of<L2>(error as L2);
+    }
+  };
 }
 
 class Right<R, L = never> extends Either<L, R> {
@@ -43,6 +54,9 @@ class Right<R, L = never> extends Either<L, R> {
 
   map = <R2>(f: (x: R) => R2): Either<L, R2> =>
     Right.of(f(this.x)) as unknown as Either<L, R2>;
+
+  mapLeft = <L2>(_: (x: L) => L2): Either<L2, R> =>
+    this as unknown as Either<L2, R>;
 
   static of = <R>(x: R) => new Right(x);
 }
@@ -64,14 +78,16 @@ class Left<L, R = never> extends Either<L, R> {
   map = <R2>(_: (x: R) => R2): Either<L, R2> =>
     this as unknown as Either<L, R2>;
 
+  mapLeft = <L2>(f: (x: L) => L2): Either<L2, R> =>
+    Left.of(f(this.x)) as unknown as Either<L2, R>;
+
   static of = <L>(x: L) => new Left(x);
 }
 
-const fromNullable = <T>(x: T) =>
-  x !== null && x !== undefined ? Right.of(x) : Left.of(1);
-
 const findColor = (name: string) =>
-  fromNullable({ red: '#ff4444', blue: '#3b5998', yellow: '#fff68f' }[name]);
+  Either.fromNullable(
+    { red: '#ff4444', blue: '#3b5998', yellow: '#fff68f' }[name]
+  );
 
 console.log(
   findColor('red')
@@ -99,3 +115,16 @@ console.log(Either.fromNullable(null));
 console.log(Either.fromNullable(undefined));
 console.log(Either.fromNullable(1).map(() => Either.fromNullable(2)));
 console.log(Either.fromNullable(1).chain(() => Either.fromNullable(2)));
+console.log(Either.fromNullable(null).chain(() => Either.fromNullable(1)));
+console.log(
+  Either.try<never, Error>(() => {
+    throw new Error('baz');
+  }).mapLeft((x) => x)
+);
+console.log(
+  Either.try<never, Error>(() => {
+    throw new Error('qux');
+  })
+    .map((x) => x)
+    .mapLeft((_) => 'an error was thrown and caught')
+);
