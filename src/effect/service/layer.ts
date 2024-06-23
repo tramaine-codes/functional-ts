b8fae4dfe51160ec5be8f7ed9ee124e69d487510
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-06-23 17:09:39 -0400

    feat: add effect layer examples

---
+++ src/effect/service/layer.ts
@@ -0,0 +1,80 @@
+import { Console, Context, Effect, Layer } from 'effect';
+
+class Config extends Context.Tag('Config')<
+  Config,
+  {
+    readonly getConfg: Effect.Effect<{
+      readonly logLevel: string;
+      readonly connection: string;
+    }>;
+  }
+>() {}
+
+class Logger extends Context.Tag('Logger')<
+  Logger,
+  {
+    readonly log: (message: string) => Effect.Effect<void>;
+  }
+>() {}
+
+class Database extends Context.Tag('Database')<
+  Database,
+  {
+    readonly query: (sql: string) => Effect.Effect<unknown>;
+  }
+>() {}
+
+const ConfigLive = Layer.succeed(Config, {
+  getConfg: Effect.succeed({
+    connection: 'mysql://username:password@hostname:port/database_name',
+    logLevel: 'INFO',
+  }),
+});
+
+const LoggerLive = Layer.effect(
+  Logger,
+  Effect.gen(function* () {
+    const config = yield* Config;
+    return {
+      log: (message) =>
+        Effect.gen(function* () {
+          const { logLevel } = yield* config.getConfg;
+          yield* Console.log(`[${logLevel}] ${message}`);
+        }),
+    };
+  })
+);
+
+const DatabaseLive = Layer.effect(
+  Database,
+  Effect.gen(function* () {
+    const config = yield* Config;
+    const logger = yield* Logger;
+    return {
+      query: (sql: string) =>
+        Effect.gen(function* () {
+          yield* logger.log(`Executing query: ${sql}`);
+          const { connection } = yield* config.getConfg;
+          return {
+            result: `Results from ${connection}`,
+          };
+        }),
+    };
+  })
+);
+
+const AppConfigLive = Layer.merge(ConfigLive, LoggerLive);
+
+const MainLive = DatabaseLive.pipe(
+  Layer.provide(AppConfigLive),
+  Layer.provide(ConfigLive)
+);
+
+const program = Effect.gen(function* () {
+  const database = yield* Database;
+  return yield* database.query('SELECT * FROM users');
+});
+
+Effect.runSync(
+  Effect.provide(program, MainLive).pipe(Effect.andThen(Console.log))
+);
