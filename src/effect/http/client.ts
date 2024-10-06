0bea7d6258cf693032ce1f70d696ee8dfeaa458a
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-10-06 19:17:48 -0400

    chore: upgrade dependencies

--- src/effect/http/client.ts
+++ src/effect/http/client.ts
@@ -1,8 +1,10 @@
 import {
+  FetchHttpClient,
   HttpClient,
   HttpClientRequest,
   HttpClientResponse,
 } from '@effect/platform';
+import { BunRuntime } from '@effect/platform-bun';
 import * as Schema from '@effect/schema/Schema';
 import { Console, Effect } from 'effect';
 import { constVoid } from 'effect/Function';
@@ -14,12 +16,17 @@ const Post = Schema.Struct({
   userId: Schema.Number,
 });
 
-await HttpClientRequest.get(
-  'https://jsonplaceholder.typicode.com/posts/1'
-).pipe(
-  HttpClient.fetchOk,
+Effect.gen(function* () {
+  const client = (yield* HttpClient.HttpClient).pipe(HttpClient.filterStatusOk);
+  const request = HttpClientRequest.get(
+    'https://jsonplaceholder.typicode.com/posts/1'
+  );
+
+  return yield* client.execute(request);
+}).pipe(
   Effect.andThen(HttpClientResponse.schemaBodyJson(Post)),
   Effect.scoped,
+  Effect.provide(FetchHttpClient.layer),
   Effect.tapBoth({
     onFailure: Console.log,
     onSuccess: Console.log,
@@ -29,19 +36,5 @@ await HttpClientRequest.get(
     onSuccess: constVoid,
   }),
   Effect.ignore,
-  Effect.runPromise
-);
-
-await HttpClientRequest.get(
-  'https://jsonplaceholder.typicode.com/posts/1'
-).pipe(
-  HttpClient.fetchOk,
-  Effect.andThen(HttpClientResponse.schemaBodyJson(Post)),
-  Effect.scoped,
-  Effect.tapBoth({
-    onFailure: Console.log,
-    onSuccess: Console.log,
-  }),
-  Effect.ignore,
-  Effect.runPromise
+  BunRuntime.runMain
 );
